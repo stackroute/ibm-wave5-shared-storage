4fa9c455dd7ef24a545c735677c97c06ec1854bc
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-07-05 10:19:07 +0530

    modified views

---
+++ smart-spaces/src/app/mynav/mynav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mynav',
+  templateUrl: './mynav.component.html',
+  styleUrls: ['./mynav.component.css']
+})
+export class MynavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
