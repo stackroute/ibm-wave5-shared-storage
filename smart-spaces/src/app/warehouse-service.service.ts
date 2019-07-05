cd5e090b713130a4426c46e3e9ec5f6acc878610
Akhila <kakhila96@gmail.com>
2019-07-05 17:36:08 +0530

    Login Register works fine

--- smart-spaces/src/app/warehouse-service.service.ts
+++ smart-spaces/src/app/warehouse-service.service.ts
@@ -19,9 +19,9 @@ export class WarehouseServiceService {
 
   // Getting values from json
   
-  // getWarehouseData(): any{
-  //     console.log("get ware house data is working))))))");
-  //     return this.Http.get("http://localhost:3000/warehouse")
-  // }
+  getWarehouseData(): any{
+      console.log("get ware house data is working))))))");
+      return this.Http.get("http://localhost:3000/warehouse")
+  }
 
 }
