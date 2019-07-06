baa25c5efd819236bb4cfc04483c7522eff3388c
sneha <lakshmisneha.516@gmail.com>
2019-07-06 12:38:32 +0530

    getting partitions related to mailid

--- booking-service/src/main/java/com/stackroute/service/BookingService.java
+++ booking-service/src/main/java/com/stackroute/service/BookingService.java
@@ -1,14 +1,18 @@
 package com.stackroute.service;
 
+import com.stackroute.exception.EmailDoesNotExists;
 import com.stackroute.model.History;
 
 import java.util.List;
 
 public interface BookingService {
 
+
     public History saveHistory(History history) throws Exception;
 
     public boolean deleteHistory(int id) throws Exception;
+
+    public  List<History> OneMailHistory(String userMailId) throws EmailDoesNotExists;
 
     public  List<History> getAllHistory() throws Exception;
 }
