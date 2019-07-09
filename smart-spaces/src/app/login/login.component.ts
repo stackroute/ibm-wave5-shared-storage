f7ed38ade2fd319f2e812ec2c2633559c6ff3647
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-07-09 16:16:20 +0530

    Booking component done

--- smart-spaces/src/app/login/login.component.ts
+++ smart-spaces/src/app/login/login.component.ts
@@ -32,19 +32,33 @@ export class LoginComponent implements OnInit {
       console.log(data)
       console.log("hiiii")
       if (data.token) {
+
+        sessionStorage.setItem('data',data);
         sessionStorage.setItem('token', data.token);
-   
         sessionStorage.setItem('details', JSON.stringify(this.helper.decodeToken(data.token)));
         console.log(this.helper.decodeToken(data.token));
-
         let type = this.helper.decodeToken(data.token).aud;
         console.log(type, "this is the role")
+        
+
+
+    
+        sessionStorage.setItem('name', this.helper.decodeToken(data.token).jti);
+        sessionStorage.setItem('mobile', this.helper.decodeToken(data.token).sub);
+        sessionStorage.setItem('mail', this.helper.decodeToken(data.token).iss);
+        sessionStorage.setItem('role', this.helper.decodeToken(data.token).aud);
+        
+     
+        console.log(sessionStorage.getItem('name')+"Inside this damn thing");
+  
+
+
       
         if (type == "true") {
-          this.myRoute.navigateByUrl("/recommendation/"+email+"/"+pwd);
+          this.myRoute.navigateByUrl("/recommendation");
         }
        if(type == "false") {
-          this.myRoute.navigateByUrl("/listed-storage/"+email+"/"+pwd);
+          this.myRoute.navigateByUrl("/listed-storage");
 
         }
       }
