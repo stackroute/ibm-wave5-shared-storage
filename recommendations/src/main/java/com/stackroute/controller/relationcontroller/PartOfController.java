3cc661fab5371cf6fe677ff7e8c0698b6f372f3d
PranitaMusande <ppmusande07@gmail.com>
2019-07-15 18:09:16 +0530

    Modified search-result-component

--- recommendations/src/main/java/com/stackroute/controller/relationcontroller/PartOfController.java
+++ recommendations/src/main/java/com/stackroute/controller/relationcontroller/PartOfController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(value = "/api/v1/recommendation")
 public class PartOfController
 {
