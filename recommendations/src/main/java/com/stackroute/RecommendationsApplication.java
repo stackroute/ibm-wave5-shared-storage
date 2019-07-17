27584109aba40a78c14cc8bed77838d2d1fa3af2
Pallavi <pallavi.p8296@gmail.com>
2019-07-17 14:16:15 +0530

    eureka dependency and annotation added

--- recommendations/src/main/java/com/stackroute/RecommendationsApplication.java
+++ recommendations/src/main/java/com/stackroute/RecommendationsApplication.java
@@ -2,10 +2,12 @@ package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @EnableNeo4jRepositories
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RecommendationsApplication {
 
 	public static void main(String[] args) {
