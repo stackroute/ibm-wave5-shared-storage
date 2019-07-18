34f7982bfbce5880e08e3a4910d4b8b115fce62a
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-07-18 10:47:27 +0530

    Added corsFilter in zuul

--- zuul-api/src/main/java/com/stackroute/zuulapi/ZuulApiApplication.java
+++ zuul-api/src/main/java/com/stackroute/zuulapi/ZuulApiApplication.java
@@ -7,15 +7,39 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableZuulProxy
+
 public class ZuulApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiApplication.class, args);
 	}
+
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
+
 
 	@Bean
 	public SimpleFilter simpleFilter(){
